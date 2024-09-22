02e6c1a101ca5d34321dab9a44e6eb4b6b46bee8
Nimal Das <nimal.das@fujitsu.com>
2024-09-22 06:14:45 +0200

    Modify root email back to original

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "nimaldasps+sandbox1@gmail.com"
+    AccountEmail              = "nimaldasps+sandbox@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "nimaldasps+sandbox@gmail.com"
